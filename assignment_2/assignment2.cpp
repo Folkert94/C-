acffcdb8329d3d7202fce815f17f925a8cc42804
Folkert Stijnman <folkertstijnman@hotmail.com>
2019-03-22 15:57:02 +0100

    Basic Compress and Decompress functionality

--- assignment_2/assignment2.cpp
+++ assignment_2/assignment2.cpp
@@ -3,8 +3,7 @@ Folkert Stijnman
 10475206
 Assignment 2, Simple Compression
 
-
-A yearsOld, monthsOld and DayofTheWeek calculator
+A compress and decompress program
 **/
 
 #include <stdio.h>
@@ -13,16 +12,30 @@ A yearsOld, monthsOld and DayofTheWeek calculator
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-void compile_stdin(std::istream& fin) {
+void compile_stdout(std::istream& fin) {
     std::string line;
+    int line_count = 0;
+    int char_in_input = 0;
+    int char_in_output = 0;
     while (getline(fin, line)) {
+        if (line.size() == 0) {
+            line_count++;
+        }
         for(int i = 0; i < line.size(); i++) {
+            char_in_input++;
+            char_in_output++;
+            if (line_count > 0) {
+                cout << '\\' << 'n' << line_count;
+                line_count = 0;
+            }
             int counter = 1;
             while (line[i] == line[i+1]) {
                 counter++;
+                char_in_input++;
                 i++;
             }
             if (counter == 1) {
@@ -43,10 +56,13 @@ void compile_stdin(std::istream& fin) {
             }
         }
     }
+    printf("\n");
+    float compr_ratio = (float)char_in_output / (float)char_in_input;
+    std::cerr << "Compression result: " << compr_ratio << '\n';
 }
 
-void compile_file(std::istream& fin) {
-    ofstream outfile("output.txt");
+void compile_to_file(std::istream& fin, const std::string& output) {
+    ofstream outfile(output);
     std::string line;
     int line_count = 0;
     int char_in_input = 0;
@@ -85,9 +101,59 @@ void compile_file(std::istream& fin) {
                 }
             }
         }
+        if (line_count == 0) {
+            line_count++;
+        }
     }
     float compr_ratio = (float)char_in_output / (float)char_in_input;
     std::cerr << "Compression result: " << compr_ratio << '\n';
+}
+
+void decompress_stdout(std::istream& fin) {
+    std::string line;
+    std::string num_occ;
+    std::string str1 ("\\");
+    std::string str2 ("\\n");
+    while (getline(fin, line)) {
+        for(int i = 0; i < line.size(); i++) {
+            std::string temp;
+            if (line[i] == '\\') {
+                temp = line[i];
+                i++;
+            }
+            temp += line[i];
+            while (isdigit(line[i + 1])) {
+                i++;
+                num_occ += line[i];
+            }
+            stringstream geek(num_occ);
+            int repeat = 0;
+            geek >> repeat;
+            num_occ.clear();
+            while (repeat > 1) {
+                if (temp.find(str2) != std::string::npos) {
+                    cout << "\n";
+                }
+                else if (temp.find(str1) != std::string::npos) {
+                    cout << temp[1];
+                }
+                else {
+                    cout << temp;
+                }
+                repeat--;
+            }
+            if (temp == "\\n") {
+                cout << '\n';
+            }
+            else if (temp.find(str1) != std::string::npos) {
+                cout << temp[1];
+            }
+            else {
+                cout << temp;
+            }
+        }
+    }
+    cout << '\n';
 }
 
 int main(int argc, char* argv[]) {
@@ -95,10 +161,16 @@ int main(int argc, char* argv[]) {
     ifstream ifn;
 
     if(argc == 1) {
-        compile_stdin(std::cin);
+        compile_stdout(std::cin);
     }
     else {
-        std::ifstream inFile(argv[1]);
-        compile_file(inFile);
+        if (argc == 3) {
+            std::ifstream inFile(argv[1]);
+            compile_to_file(inFile, argv[2]);
+        }
+        else {
+            std::ifstream inFile(argv[1]);
+            decompress_stdout(inFile);
+        }
     }
 }
